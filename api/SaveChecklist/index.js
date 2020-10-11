2fb5620514b4337ad6869b4b0bd1f355688ea913
sebug <sebastian.gfeller@gmail.com>
2020-10-11 23:14:09 +0200

    Adding mail only when posted to

--- api/SaveChecklist/index.js
+++ api/SaveChecklist/index.js
@@ -4,24 +4,31 @@ const sgMail = require('@sendgrid/mail')
 module.exports = async function (context, req) {
     context.log("Returns the result of the function.");
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-        to: process.env.TO_EMAIL, // Change to your recipient
-        from: process.env.FROM_EMAIL, // Change to your verified sender
-        subject: 'Contrôle construction',
-        text: 'Easy text',
-        html: '<strong>easy</string> text',
-    };
-    await sgMail.send(msg);
-    context.log('e-mail sent');
-
     const body = req.rawBody;
     // Retrieve the boundary id
     const boundary = multipart.getBoundary(req.headers["content-type"]);
     if (boundary) {
       const parts = multipart.parse(body, boundary);
   
-      context.log(parts);
+      context.log('The length is ' + parts.length);
+      context.log(JSON.stringify(parts[0]));
+
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+      const msg = {
+          to: process.env.TO_EMAIL, // Change to your recipient
+          from: process.env.FROM_EMAIL, // Change to your verified sender
+          subject: 'Contrôle construction',
+          text: 'Easy text'
+      };
+
+      let text = 'Compte-rendu contrôle construction';
+      let html = '<p>Compte-rendu contrôle construction</p>';
+
+      msg.text = text;
+      msg.html = html;
+
+      await sgMail.send(msg);
+      context.log('e-mail sent');
   
       context.res.status(200);
       context.res.body = 'Formulaire envoyé';
