92082e02644682191ca82d1131028f000503364a
sebug <sebastian.gfeller@gmail.com>
2020-10-11 21:53:40 +0200

    Trying to parse body

--- api/SaveChecklist/index.js
+++ api/SaveChecklist/index.js
@@ -1,8 +1,18 @@
-module.exports = async function (context, req) {
-    context.log('Invoked function to save checklist.');
+import * as multipart from 'parse-multipart';
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: req.body
-    };
+module.exports = async function (context, req) {
+    const body = req.rawBody;
+    // Retrieve the boundary id
+    const boundary = multipart.getBoundary(req.headers["content-type"]);
+    if (boundary) {
+      const files = multipart.Parse(Buffer.from(body), boundary);
+  
+      if (files && files.length > 0) {
+        // Do what you want to do with the file
+      }
+  
+      context.res.status(200);
+    } else {
+      context.res.status(500).send("No file(s) found.");
+    }
 }
