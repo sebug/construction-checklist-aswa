1f2123fbc1a38155fd43715680592b2ef35de13d
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-17 18:29:26 +0000

    exception response

--- api/CheckInTrigger/index.js
+++ api/CheckInTrigger/index.js
@@ -1,8 +1,7 @@
 module.exports = async function (context, req) {
-    const d = new Date();
-    context.log('Checking in on construction ' + req.query.construction + ' at ' + d.toISOString());
-
     try {
+        const d = new Date();
+        context.log('Checking in on construction ' + req.query.construction + ' at ' + d.toISOString());
         context.bindings.tableBinding = [];
 
         context.bindings.tableBinding.push({
@@ -11,15 +10,21 @@ module.exports = async function (context, req) {
             "Date": d.toISOString(),
             "Construction": d.toISOString()
         });
+
+
+        context.res = {
+            status: 302,
+            headers: {
+                "Location": "/?construction=" + encodeURIComponent(req.query.construction)
+            },
+            body: null
+        };
     } catch (e) {
         context.log(e);
-    }
 
-    context.res = {
-        status: 302,
-        headers: {
-            "Location": "/?construction=" + encodeURIComponent(req.query.construction)
-        },
-        body: null
-    };
+        context.res = {
+            status: 500,
+            body: '' + e
+        };
+    }
 }
