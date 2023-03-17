b9479414ae6b9a0cef0fd315c6a6b57727cb5f62
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-17 18:27:56 +0000

    Try cach add to azure tables

--- api/CheckInTrigger/index.js
+++ api/CheckInTrigger/index.js
@@ -15,7 +15,7 @@ module.exports = async function (context, req) {
         context.log(e);
     }
 
-    context.bindings.res = {
+    context.res = {
         status: 302,
         headers: {
             "Location": "/?construction=" + encodeURIComponent(req.query.construction)
