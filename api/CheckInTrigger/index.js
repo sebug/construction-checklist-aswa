e82e635f79d7f249706a29a48d7c5a0dbbf96ecf
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-17 17:24:32 +0000

    with location

--- api/CheckInTrigger/index.js
+++ api/CheckInTrigger/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('Checking in on construction ' + req.query.construction + ' at ' + new Date());
 
     context.res = {
-        status: 307,
+        status: 302,
         headers: {
             "Location": "/?construction=" + encodeURIComponent(req.query.construction)
         },
