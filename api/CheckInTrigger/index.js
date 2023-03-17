8a05db771074e109923a0fec370edb149ae06c2e
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-17 18:22:41 +0000

    Try catching checkin log

--- api/CheckInTrigger/index.js
+++ api/CheckInTrigger/index.js
@@ -2,14 +2,18 @@ module.exports = async function (context, req) {
     const d = new Date();
     context.log('Checking in on construction ' + req.query.construction + ' at ' + d.toISOString());
 
-    context.bindings.tableBinding = [];
+    try {
+        context.bindings.tableBinding = [];
 
-    context.bindings.tableBinding.push({
-        "PartitionKey": "Prod",
-        "RowKey": req.query.construction + "_" + d.toISOString(),
-        "Date": d.toISOString(),
-        "Construction": d.toISOString()
-    });
+        context.bindings.tableBinding.push({
+            "PartitionKey": "Prod",
+            "RowKey": req.query.construction + "_" + d.toISOString(),
+            "Date": d.toISOString(),
+            "Construction": d.toISOString()
+        });
+    } catch (e) {
+        context.log(e);
+    }
 
     context.bindings.res = {
         status: 302,
