6ae82db2661339fac2bfa43a2b1b3b7ec5e72777
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-17 19:42:44 +0000

    Adding checkin entry

--- api/CheckInTrigger/index.js
+++ api/CheckInTrigger/index.js
@@ -1,16 +1,42 @@
+const { TableServiceClient, AzureNamedKeyCredential, TableClient } = require("@azure/data-tables");
+
 module.exports = async function (context, req) {
     try {
+        const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
+        const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
+        const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
+
         const d = new Date();
         context.log('Checking in on construction ' + req.query.construction + ' at ' + d.toISOString());
-        context.bindings.tableBinding = [];
 
-        context.bindings.tableBinding.push({
-            "partitionKey": "Prod",
-            "rowKey": req.query.construction + "_" + d.toISOString(),
-            "Date": d.toISOString(),
-            "Construction": d.toISOString()
+        const url = 'https://' + account + '.table.' + suffix;
+    
+        const credential = new AzureNamedKeyCredential(account, accountKey);
+        const serviceClient = new TableServiceClient(
+            url,
+            credential
+        );
+    
+        const tableName = 'checkins';
+
+        await serviceClient.createTable(tableName, {
+            onResponse: (response) => {
+                if (response.status === 409) {
+                    context.log('Table checkins already exists');
+                }
+            }
         });
 
+        const tableClient = new TableClient(url, tableName, credential);
+        const rowKey = d.toISOString();
+
+        let entity = {
+            partitionKey: "Prod",
+            rowKey: rowKey,
+            StartDate: d.toISOString(),
+            Construction: req.query.construction
+        };
+        await tableClient.insertEntity(entity);
 
         context.res = {
             status: 302,
