d9f029ffda0261fb3a901cfdc5a53da097def0c4
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-17 18:06:38 +0000

    Multiple output bindings

--- api/CheckInTrigger/index.js
+++ api/CheckInTrigger/index.js
@@ -1,5 +1,15 @@
 module.exports = async function (context, req) {
-    context.log('Checking in on construction ' + req.query.construction + ' at ' + new Date());
+    const d = new Date();
+    context.log('Checking in on construction ' + req.query.construction + ' at ' + d.toISOString());
+
+    context.bindings.tableBinding = [];
+
+    context.bindings.tableBinding.push({
+        "PartitionKey": "Prod",
+        "RowKey": req.query.construction + "_" + d.toISOString(),
+        "Date": d.toISOString(),
+        "Construction": d.toISOString()
+    });
 
     context.res = {
         status: 302,
