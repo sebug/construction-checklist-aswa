f4ae60e9eb06304225377c45f82dc11dfd08c70b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-10-04 12:06:47 +0000

    Adding a response

--- api/CheckAccessCodeTrigger/index.js
+++ api/CheckAccessCodeTrigger/index.js
@@ -1,3 +1,49 @@
+const { TableServiceClient, AzureNamedKeyCredential, TableClient } = require("@azure/data-tables");
+
+const checkAccessCodeValidity = async (context, construction, accessCode) => {
+    const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
+    const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
+    const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
+
+    try {
+        const credential = new AzureNamedKeyCredential(account, accountKey);
+        const serviceClient = new TableServiceClient(
+            url,
+            credential
+        );
+    
+        const tableName = 'constructions';
+        await serviceClient.createTable(tableName, {
+            onResponse: (response) => {
+                if (response.status === 409) {
+                    context.log('Table constructions already exists');
+                }
+            }
+        });
+    
+        const tableClient = new TableClient(url, tableName, credential);
+        const rowKey = construction;
+    
+        let retrievePromise = new Promise((resolve, reject) => {
+            tableClient.retrieveEntity(tableName, 'prod', construction.toLowerCase(), (error, result, response) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                resolve(result);
+            });
+        });
+        let entityResult = await retrievePromise;
+        if (!entityResult) {
+            return false;
+        }
+        return entityResult;
+    } catch (err) {
+        context.log(err);
+        return false;
+    }
+};
+
 module.exports = async function (context, req) {
     context.log('Checking access code...');
 
@@ -6,8 +52,10 @@ module.exports = async function (context, req) {
 
     context.log(`Checking with construction ${construction} and access code ${accessCode}`);
 
+    
+
     const responseMessage = {
-        isValid: Number(accessCode) === 123
+        entity: await checkAccessCodeValidity(context, construction, accessCode)
     };
 
     context.res = {
