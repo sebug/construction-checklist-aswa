1098a8b7b39ba12acfc286a66a0f8725f8be40b7
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-10-06 09:03:18 +0000

    Client-side compression

--- app/js/app.js
+++ app/js/app.js
@@ -55,3 +55,72 @@ const checkAccessCode = async () => {
 accessCodeInput.addEventListener('change', checkAccessCode);
 nameOfConstructionInput.addEventListener('change', checkAccessCode);
 
+// Compress images before upload https://pqina.nl/blog/compress-image-before-upload/#saving-the-compressed-image-back-to-the-file-input
+const compressImage = async (file, { quality = 1, type = file.type }) => {
+    // Get as image data
+    const imageBitmap = await createImageBitmap(file);
+
+    // Draw to canvas
+    const canvas = document.createElement('canvas');
+
+    const maxWidth = 400;
+    let width = imageBitmap.width;
+    let height = imageBitmap.height;
+    if (width > maxWidth) {
+        height = height * (maxWidth / width);
+        width = maxWidth;
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(imageBitmap, 0, 0, width, height);
+
+    console.log(`Drawing to size ${width}x${height} (from ${imageBitmap.width}x${imageBitmap.height})`);
+
+    // Turn into Blob
+    const blob = await new Promise((resolve) =>
+        canvas.toBlob(resolve, type, quality)
+    );
+
+    // Turn Blob into File
+    return new File([blob], file.name, {
+        type: blob.type,
+    });
+};
+
+const fileInputs = Array.from(document.querySelectorAll('input[type="file"]'));
+for (const fileInput of fileInputs) {
+    fileInput.addEventListener('change', async (e) => {
+        // Get the files
+        const { files } = e.target;
+
+        // No files selected
+        if (!files.length) return;
+
+        // We'll store the files in this data transfer object
+        const dataTransfer = new DataTransfer();
+
+        // For every file in the files list
+        for (const file of files) {
+            // We don't have to compress files that aren't images
+            if (!file.type.startsWith('image')) {
+                // Ignore this file, but do add it to our result
+                dataTransfer.items.add(file);
+                continue;
+            }
+
+            // We compress the file a bit
+            const compressedFile = await compressImage(file, {
+                quality: 0.9,
+                type: 'image/jpeg',
+            });
+
+            // Save back the compressed file instead of the original file
+            dataTransfer.items.add(compressedFile);
+        }
+
+        // Set value of the file input to our new files list
+        e.target.files = dataTransfer.files;
+    });
+}
