198dda3e8a5a506e63bf2c285e34db0166c51ba5
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-09-23 12:17:22 +0000

    Sort by name

--- api/GetChecklists/index.js
+++ api/GetChecklists/index.js
@@ -57,8 +57,25 @@ module.exports = async function (context, req) {
 
         let constructionsList = [];
         for await (const entity of constructionsIter) {
-            constructionsList.push(entity);
+            constructionsList.push({
+                name: entity.rowKey,
+                accessCode: entity.AccessCode
+            });
         }
+
+        constructionsList.sort((a, b) => {
+            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+            return -1;
+            }
+            if (nameA > nameB) {
+            return 1;
+            }
+        
+            // names must be equal
+            return 0;
+        });
 
         let resultObject = {
             constructions: constructionsList
