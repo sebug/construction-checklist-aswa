02b3054260144d3244bcf66ec4057e6744700bbb
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-09-23 13:08:25 +0000

    Table header

--- app/js/liste.js
+++ app/js/liste.js
@@ -40,12 +40,40 @@ async function getList() {
 
 function constructCheckinsDetail(getListObject) {
     const details = document.createElement('details');
+    details.setAttribute('open', 'true');
     const summary = document.createElement('summary');
     summary.innerHTML = 'Contrôles des Constructions';
     details.appendChild(summary);
     const descriptionParagraph = document.createElement('p');
     descriptionParagraph.innerHTML = 'Ci-dessous vous trouvez une liste des contrôles des constructions';
     details.appendChild(descriptionParagraph);
+
+    const table = document.createElement('table');
+
+    const thead = document.createElement('thead');
+
+    const headerTr = document.createElement('tr');
+
+    const dateTh = document.createElement('th');
+    dateTh.innerHTML = 'Date';
+
+    headerTr.appendChild(dateTh);
+
+    const constructionTh = document.createElement('th');
+    constructionTh.innerHTML = 'Construction';
+
+    headerTr.appendChild(constructionTh);
+
+    thead.appendChild(headerTr);
+
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+
+    table.appendChild(tbody);
+
+    details.appendChild(table);
+
     return details;
 }
 
