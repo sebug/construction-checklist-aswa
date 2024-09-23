fa7826a4a0855f6528efc9f7cd814dcb8fd39dab
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-09-23 10:18:39 +0000

    Access code check

--- app/js/liste.js
+++ app/js/liste.js
@@ -1,1 +1,34 @@
 console.log('List part');
+
+async function getList() {
+    let getListResponse = await fetch('/api/GetChecklists');
+    if (getListResponse.status !== 200) {
+        let loginDiv = document.createElement('div');
+        loginDiv.setAttribute('class', 'login');
+        loginDiv.innerHTML = '<h2>Besoin d\'un code d\'accès</h2>' +
+        '<p>Entrez le code d\'accès pour voir la liste: <input type="password" id="accesscode" /> <button id="submitaccesscode">Afficher</button>' +
+        '</p>';
+        loginDiv.querySelector('#submitaccesscode').addEventListener('click', () => {
+            let accessCode = document.querySelector('#accesscode');
+            fetch('/api/GetChecklists', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    accessCode: accessCode.value
+                })
+            }).then(clResponse => {
+                if (clResponse.status === 200) {
+                    location.reload();
+                    return;
+                }
+                alert('Mauvais code d\'accès');
+            })
+        });
+        let mainElement = document.querySelector('main');
+        mainElement.appendChild(loginDiv);
+    }
+}
+
+getList();
