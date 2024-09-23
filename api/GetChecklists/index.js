fb5081a35898203fc0d173c5f68f0c4925130201
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-09-23 12:20:43 +0000

    Filtering out tests

--- api/GetChecklists/index.js
+++ api/GetChecklists/index.js
@@ -63,14 +63,16 @@ module.exports = async function (context, req) {
             });
         }
 
+        constructionsList = constructionsList.filter(dto => dto.name.indexOf('test') < 0);
+
         constructionsList.sort((a, b) => {
             const nameA = a.name.toUpperCase(); // ignore upper and lowercase
             const nameB = b.name.toUpperCase(); // ignore upper and lowercase
             if (nameA < nameB) {
-            return -1;
+                return -1;
             }
             if (nameA > nameB) {
-            return 1;
+                return 1;
             }
         
             // names must be equal
