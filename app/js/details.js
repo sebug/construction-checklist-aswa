e1a5141bf9e75887c0bfc52ce6c93053ab56c619
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-09-26 12:48:25 +0000

    Returning object

--- app/js/details.js
+++ app/js/details.js
@@ -1,1 +1,13 @@
 console.log('details');
+
+async function getDetails() {
+    let searchParams = new URLSearchParams(location.search);
+    const getEntityResponse = await fetch('/api/ChecklistReportTrigger?partitionKey=' +
+        searchParams.get('partitionKey') + '&rowKey=' +
+        searchParams.get('rowKey')
+    );
+    const construction = await getEntityResponse.json();
+    console.log(construction);
+}
+
+getDetails();
