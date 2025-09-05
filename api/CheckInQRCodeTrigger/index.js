a93e8cb4768f2288bc4f80b35b7b91f363dd7f39
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2025-09-05 11:45:30 +0000

    Adding proof key to QR code

--- api/CheckInQRCodeTrigger/index.js
+++ api/CheckInQRCodeTrigger/index.js
@@ -1,10 +1,44 @@
 var QRCode = require('qrcode');
+const { TableServiceClient, AzureNamedKeyCredential, TableClient, TableQuery } = require("@azure/data-tables");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const url = process.env.BASE_URL + '/api/CheckInTrigger?construction=' +
     encodeURIComponent(req.query.construction);
+
+    const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
+    const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
+    const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
+
+    try {
+        const url = 'https://' + account + '.table.' + suffix;
+
+        const credential = new AzureNamedKeyCredential(account, accountKey);
+        const serviceClient = new TableServiceClient(
+            url,
+            credential
+        );
+    
+        const tableName = 'constructions';
+        await serviceClient.createTable(tableName, {
+            onResponse: (response) => {
+                if (response.status === 409) {
+                    context.log('Table constructions already exists');
+                }
+            }
+        });
+    
+        const tableClient = new TableClient(url, tableName, credential);
+        const rowKey = construction.toLowerCase();
+
+        let entityResult = await tableClient.getEntity('prod', rowKey);
+        if (entityResult && entityResult.ProofKey) {
+            url += '&proofKey=' + encodeURIComponent(entityResult.ProofKey);
+        }
+    } catch (err) {
+        context.error(err);
+    }
 
     const opts = {
         type: 'image/png'
